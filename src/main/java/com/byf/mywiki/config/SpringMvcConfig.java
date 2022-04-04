93d90b0758af9042903d3f26ae3ef6f0fc1254dc
Administrator <949284685@qq.com>
2022-04-04 15:21:00 +0800

    9-13 增加前端路由校验

--- src/main/java/com/byf/mywiki/config/SpringMvcConfig.java
+++ src/main/java/com/byf/mywiki/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.byf.mywiki.config;
 
+import com.byf.mywiki.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,26 +10,26 @@ import javax.annotation.Resource;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//    @Resource
-//    LoginInterceptor loginInterceptor;
+    @Resource
+    LoginInterceptor loginInterceptor;
 
 //    @Resource
 ////    ActionInterceptor actionInterceptor;
 
     public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(loginInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns(
-//                        "/test/**",
-//                        "/redis/**",
-//                        "/user/login",
-//                        "/category/all",
-//                        "/ebook/list",
-//                        "/doc/all/**",
-//                        "/doc/vote/**",
-//                        "/doc/find-content/**",
-//                        "/ebook-snapshot/**"
-//                );
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
 
 //        registry.addInterceptor(actionInterceptor)
 //                .addPathPatterns(
